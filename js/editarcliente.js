fb66803f347db16b48755f6e9635629ecc2d6d9c
Hugo <hhugo.avila97@gmail.com>
2023-10-09 09:39:30 -0300

    feat: ✨ Funcion para editar cliente y guardar cambios

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,4 +1,5 @@
-import { obtenerCliente } from "./API.js";
+import { editarCliente, obtenerCliente } from "./API.js";
+import { mostrarAlerta, validar } from "./funciones.js";
 
 (function () {
 
@@ -16,6 +17,10 @@ import { obtenerCliente } from "./API.js";
         const cliente = await obtenerCliente(idCliente);
 
         mostrarCliente(cliente);
+
+        //Submit al formulario 
+        const formulario = document.querySelector('#formulario');
+        formulario.addEventListener('submit', validarCliente);
     })
 
     function mostrarCliente(cliente) {
@@ -26,8 +31,27 @@ import { obtenerCliente } from "./API.js";
         emailInput.value = email;
         telefonoInput.value = telefono;
         idInput.value = id;
-        
+    }
 
+    function validarCliente(e) {
+        e.preventDefault();
+
+        const cliente = {
+            nombre: nombreInput.value, 
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: parseInt(idInput.value)
+        }
+
+        if(validar(cliente)) {
+            //Mostrar mensaje
+            mostrarAlerta('Todos los campos son obligatorios');
+            return;
+        }
+
+        //Reescribe el objeto
+        editarCliente(cliente);
     }
 }
 )();
